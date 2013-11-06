ca7eb5f8a5c1784878be524e8f166bd01a524811
m.benchchaoui@cloudbau.de <m.benchchaoui@cloudbau.de>
2013-11-06 12:08:38 +0100

    Change cookbook version

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,8 @@
-maintainer       "Opscode, Inc."
-maintainer_email "matt@opscode.com"
+maintainer       "Cloudbau GmbH"
 license          "Apache 2.0"
 description      "The OpenStack Compute service Nova."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "5.0.0"
+version          "6.0.0"
 
 recipe		  "api-ec2", ""
 recipe		  "api-metadata", ""
